5fdb0669085ccaa7a510047de6c95ca2957e0576
gmouky <gmouky@github.com>
2023-08-19 16:15:28 +0200

    creation of create blog post rest api

---
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,44 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.dto.PostDto;
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        //convertDto to Entity
+        Post post = mapDtoToEntity(postDto);
+
+        Post newPost = postRepository.save(post);
+
+        //convert entity to Dto and return it
+        return mapEntityToDto(newPost);
+    }
+
+    private Post mapDtoToEntity(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
+    }
+
+    private PostDto mapEntityToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
+    }
+}
