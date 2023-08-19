3f3998f8a79b5b65c3ff052e942cbe43a906ba61
gmouky <gmouky@github.com>
2023-08-19 16:33:43 +0200

    implementing get all post rest api

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -6,6 +6,9 @@ import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -22,7 +25,15 @@ public class PostServiceImpl implements PostService {
         Post newPost = postRepository.save(post);
 
         //convert entity to Dto and return it
-        return mapEntityToDto(newPost);
+        PostDto postResponse =  mapEntityToDto(newPost);
+
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(post -> mapEntityToDto(post)).collect(Collectors.toList());
     }
 
     private Post mapDtoToEntity(PostDto postDto) {
