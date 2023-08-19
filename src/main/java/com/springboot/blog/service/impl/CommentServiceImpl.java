b239c2e1e30bba71ee105fc3a3e3fa5531480181
gmouky <gmouky@github.com>
2023-08-19 20:18:42 +0200

    create rest api for adding a new comment

---
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -0,0 +1,56 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.dto.CommentDto;
+import com.springboot.blog.dto.PostDto;
+import com.springboot.blog.entity.Comment;
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.repository.CommentRepository;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.CommentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private CommentRepository commentRepository;
+    private PostRepository postRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public CommentDto createComment(long postId, CommentDto commentDto) {
+        Comment comment = mapDtoToEntity(commentDto);
+
+        //retrieve post entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        //set post to comment entity
+        comment.setPost(post);
+
+        //save comment entity to the db
+        Comment newComment = commentRepository.save(comment);
+        return mapEntityToDto(newComment);
+    }
+
+    private Comment mapDtoToEntity(CommentDto commentDto) {
+        Comment comment = new Comment();
+        comment.setId(comment.getId());
+        comment.setBody(commentDto.getBody());
+        comment.setName(commentDto.getName());
+        comment.setEmail(commentDto.getEmail());
+        return comment;
+    }
+
+    private CommentDto mapEntityToDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setBody(comment.getBody());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        return commentDto;
+    }
+}
