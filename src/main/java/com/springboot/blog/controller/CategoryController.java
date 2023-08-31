e8c71ae16ebce779c0aff3baaf88102cf92e9d33
gmouky <gmouky@github.com>
2023-08-31 22:46:07 +0200

    create get category by id, get all and update categor Rest API

--- src/main/java/com/springboot/blog/controller/CategoryController.java
+++ src/main/java/com/springboot/blog/controller/CategoryController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
@@ -17,10 +19,29 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto) {
        CategoryDto category = categoryService.addCategory(categoryDto);
         return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> getCategory(@PathVariable("id") long categoryId) {
+       CategoryDto categoryDto = categoryService.getCategory(categoryId);
+       return new ResponseEntity<>(categoryDto, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> getCategories() {
+        return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto,
+                                                      @PathVariable("id") long categoryId) {
+        CategoryDto updatedCategory = categoryService.updateCategory(categoryId, categoryDto);
+        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
     }
 }
--- src/main/java/com/springboot/blog/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/CategoryServiceImpl.java
@@ -2,11 +2,14 @@ package com.springboot.blog.service.impl;
 
 import com.springboot.blog.dto.CategoryDto;
 import com.springboot.blog.entity.Category;
+import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.repository.CategoryRepository;
 import com.springboot.blog.service.CategoryService;
-import com.sun.jdi.PrimitiveValue;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -26,5 +29,37 @@ public class CategoryServiceImpl implements CategoryService {
         Category savedCatedory = categoryRepository.save(category);
 
         return modelMapper.map(savedCatedory, CategoryDto.class);
+    }
+
+    @Override
+    public CategoryDto getCategory(Long categoryId) {
+        Category category = getCategoryById(categoryId);
+        return modelMapper.map(category, CategoryDto.class);
+    }
+
+    @Override
+    public List<CategoryDto> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream()
+                .map(category -> modelMapper.map(category, CategoryDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDto updateCategory(long id, CategoryDto categoryDto) {
+        Category category = getCategoryById(id);
+
+        category.setName(categoryDto.getName());
+        category.setDescription(category.getDescription());
+        category.setId(id);
+
+        Category updatedCategory = categoryRepository.save(category);
+
+        return modelMapper.map(updatedCategory, CategoryDto.class);
+    }
+
+    private Category getCategoryById(long categoryId) {
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("category", "id", categoryId));
     }
 }
