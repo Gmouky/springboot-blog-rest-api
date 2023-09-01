182e5de70d3423721efe8cfc917b0fdba26091f7
gmouky <gmouky@github.com>
2023-09-01 23:26:48 +0200

    add API information using Annotation

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,36 @@
 package com.springboot.blog;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "spring boot Blog App REST APIs",
+				description = "Spring boot blog App REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "mouky",
+						email = "mouky@mouky.com",
+						url = "https//www.test.com"
+				),
+				license = @License(
+						name = "apache 3.0",
+						url = "http://test.com/licence"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/Gmouky/springboot-blog-rest-api"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 
 	@Bean
