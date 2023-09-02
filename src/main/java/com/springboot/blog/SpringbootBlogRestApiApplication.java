26e49997b5f1695037a7c75d0eccda0f47b2d5ac
gmouky <gmouky@github.com>
2023-09-02 10:03:11 +0200

    add a code to insert metadata in table. can be done via data.sql, schema.sql too

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,37 +1,21 @@
 package com.springboot.blog;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "spring boot Blog App REST APIs",
-				description = "Spring boot blog App REST API Documentation",
-				version = "v1.0",
-				contact = @Contact(
-						name = "mouky",
-						email = "mouky@mouky.com",
-						url = "https//www.test.com"
-				),
-				license = @License(
-						name = "apache 3.0",
-						url = "http://test.com/licence"
-				)
-		),
-		externalDocs = @ExternalDocumentation(
-				description = "Spring Boot Blog App Documentation",
-				url = "https://github.com/Gmouky/springboot-blog-rest-api"
-		)
-)
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper() {
@@ -42,4 +26,17 @@ public class SpringbootBlogRestApiApplication {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+	@Override
+	public void run(String... args) throws Exception {
+
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
